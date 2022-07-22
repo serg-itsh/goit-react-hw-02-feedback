cc8e15cdb927edc4ecb70fb59169dde88fdcfcf4
Sergey Shevchenko <sergeishevchenko111@gmail.com>
2022-07-22 23:44:48 +0300

    Початкова розмітка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FeedbackList } from './FeedbackList/FeedbackList';
+
+export class App extends Component {
+  // static defaultProps = {
+  //   initialValue: 0,
+  // }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  // handleClick = () => {
+  //   this.setState({ good: this.state.counter + 1 });
+  // }
+
+  handleClickGood = event => {
+    console.log(event.target);
+    // const { name } = event.target;
+    this.setState(prevState => {
+      return {
+        // [name]: preVal[name] + 1
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  handleClickNeutral = event => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  handleClickBad = event => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        {/* <FeedbackList option={option} /> */}
+        <FeedbackList
+          option={Object.keys(this.state)}
+          onHandleClickGood={this.handleClickGood}
+          onHandleClickNeutral={this.handleClickNeutral}
+          onHandleClickBad={this.handleClickBad}
+        />
+        {/* <Statistic> */}
+        <h1>Statistics</h1>
+        <div className="text">
+          <p> Good: {good}</p>
+          <p> Neutral: {neutral}</p>
+          <p> Bad: {bad}</p>
+        </div>
+        {/* </Statistic> */}
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackList/FeedbackList.jsx
@@ -0,0 +1,41 @@
+// import React from "react";
+import PropTypes from 'prop-types';
+import './FeedbackList.css';
+
+export const FeedbackList = ({
+  option,
+  onHandleClickGood,
+  onHandleClickNeutral,
+  onHandleClickBad,
+}) => {
+  return (
+    <div className="list">
+      <h1>Please leave feedback</h1>
+      <div className="list-item">
+        <button type="button" className="btn" onClick={onHandleClickGood}>
+          Good
+        </button>
+        <button type="button" className="btn" onClick={onHandleClickNeutral}>
+          Neutral
+        </button>
+        <button type="button" className="btn" onClick={onHandleClickBad}>
+          Bad
+        </button>
+      </div>
+
+      {/* <h1>Statistics</h1>
+      <div>
+        <p> Good: {option}</p>
+        <p> Neutral: {}</p>
+        <p> Bad: {}</p>
+        
+        </div> */}
+    </div>
+  );
+};
+
+FeedbackList.propTypes = {
+  option: PropTypes.arrayOf(PropTypes.string.isRequired),
+  handleClick: PropTypes.func.isRequired,
+};
+// export default FeedbackList;
